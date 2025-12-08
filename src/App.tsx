73c3c20d88dc8d90c503d5717ee84ee05446645f
Isabel Gómez <mariaisabel201011@gmail.com>
2025-12-07 19:03:51 -0500

    Add handler for attempts

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
+import { useState } from 'react';
 import './App.css'
 import { HangImage } from './components/HangImage'
 import { letters } from './helpers/letters'
 
 function App() {
 
+  const [attempts, setAttempts] = useState<number>(0);
+
+  const checkLetter = (letter: string) => {
+    //Math se usa para que en pantalla, en los intentos, no aparezca un número mayor a 9 
+    setAttempts(Math.min(attempts + 1, 9));
+  }
+
   return (
     <div className="App">
       {/* Imagenes */}
-      <HangImage imageNumber={4}/>
+      <HangImage imageNumber={attempts} />
 
       {/* Palabra oculta */}
       <h3>_ _ _ _ _</h3>
 
       {/* Contador de intentos */}
-      <h3> Intentos: 0</h3>
+      <h3> Intentos: {attempts}</h3>
 
       {/* Teclado letras */}
       {
         letters.map((letter) => (
-          <button key={letter}>
+          <button
+            onClick={() => checkLetter(letter)}
+            key={letter}
+          >
             {letter}
-            </button>
+          </button>
         ))
       }
     </div>
